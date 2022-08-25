ac40fb5d9a8709ab4f048ca4dfc0fdfb13793a04
esmithzb <esmithzb@mathworks.com>
2022-08-25 14:40:52 -0400

    Modify navigation process to apply more cleanly
    1) When "Return to Overview" arrows are used in scripts, close the
       script as you as leaving.
    2) When the project runs the first time, open the navigation splash page
       without closing anything.

--- HelperFunctions/Startup.m
+++ HelperFunctions/Startup.m
@@ -1,14 +1,10 @@
-function OpenOverview
+function Startup
 % Open the overview file
-
 locDir = pwd;
 if contains(locDir,filesep+"MATLAB Drive")
     open("NavigationOverview.mlx")
 else
     open("Overview.html")
 end
-
-% Close the current script
-close(matlab.desktop.editor.getActive)
 
 end
