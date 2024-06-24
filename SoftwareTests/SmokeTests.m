d6eb22a8522a1144ada3bda047eb144f3342deb4
Emma Smith Zbarsky <esmithzb@mathworks.com>
2024-06-24 16:36:55 -0400

    Fix SmokeTests for Arrays and Algorithms

--- SoftwareTests/SmokeTests.m
+++ SoftwareTests/SmokeTests.m
@@ -1,108 +1,120 @@
-% Run these tests with runMyTests
-% All tests so far are on code expected to run without errors
-% If/when we end up with a version that _should_ error,
-% please add it to this set of examples
 classdef SmokeTests < matlab.unittest.TestCase
-
-    properties
-        rootProject
-        results
+    
+    properties (ClassSetupParameter)
+        Project = {''};
     end
 
+    properties (TestParameter)
+        Scripts;
+    end
+
+    methods (TestParameterDefinition,Static)
+
+        function Scripts = GetScriptName(Project)
+            RootFolder = currentProject().RootFolder;
+            Scripts = dir(fullfile(RootFolder,"Scripts","*.mlx"));
+            Scripts = {Scripts.name};
+        end
+
+    end
 
     methods (TestClassSetup)
 
-        function setUpPath(testCase)
-
+        function SetUpSmokeTest(testCase,Project)
             try
-                project = currentProject;
-                testCase.rootProject = project.RootFolder;
-                cd(testCase.rootProject)
-            catch
-                error("Load project prior to run tests")
+               currentProject;  
+            catch ME
+                warning("Project is not loaded.")
             end
-
-            testCase.log("Running in " + version)
-
-        end % function setUpPath
-
-        function setUpResults(testCase)
-            files = dir(fullfile(testCase.rootProject,"Scripts","*.mlx"));
-            testCase.results = struct;
-            testCase.results.Name = strings(size(files));
-            testCase.results.Passed = false(size(files));
-            testCase.results.Time = zeros(size(files));
-            testCase.results.Message = strings(size(files));
-            for k = 1:length(files)
-                testCase.results.Name(k) = string(files(k).name);
-            end
-
-        end % function setUpResults
-
-    end % methods (TestClassSetup)
-
-    methods(Test)
-
-        function smokeTest(testCase)
-            myFiles = testCase.results.Name;
-            fid = fopen(fullfile("SoftwareTests","TestResults_"+release_version+".txt"),"w");
-            fprintf(fid,"Version,File,Status,ElapsedTime\n");
-            for kTest = 1:length(myFiles)
-                [flag,msg,time] = SmokeTestMyFile(myFiles(kTest));
-                testCase.results.Time(kTest) = time;
-                if flag
-                    testCase.results.Time(kTest) = toc;
-                    disp("Failed " + myFiles(kTest) + " because " + ...
-                        newline + msg)
-                    testCase.results.Message(kTest) = msg;
-                    fprintf(fid,"%s,%s,%s,%s\n",release_version,myFiles(kTest),"failed",testCase.results.Time(kTest));
-                else
-                    disp("Finished " + myFiles(kTest))
-                    testCase.results.Passed(kTest) = true;
-                    fprintf(fid,"%s,%s,%s,%s\n",release_version,myFiles(kTest),"passed",testCase.results.Time(kTest));
-                end
-            end
-            fclose(fid);
-            struct2table(testCase.results)
-            assert(all([testCase.results.Passed]))
         end
 
+    
+    end
+
+
+    
+    methods(Test)
+
+        function SmokeRun(testCase,Scripts)
+            Filename = string(Scripts);
+            switch (Filename)
+                case "Introduction.mlx"
+                    SmokeTestWithError(testCase,Filename,"Unrecognized function or variable 'Monday'.")
+                otherwise
+                    SimpleSmokeTest(testCase,Filename)
+            end
+        end
+            
+    end
+
+
+    methods (Access = private)
+
+        function SmokeTestWithError(testCase,Filename,MyError)
+            % Run the Smoke test
+            RootFolder = currentProject().RootFolder;
+            cd(RootFolder)
+            disp(">> Running " + Filename);
+            try
+                run(fullfile("Scripts",Filename));
+            catch ME
+                if ME.message == MyError
+                    MyErrorMessage = "Expected error.";
+                else
+                MyErrorMessage = ME.message;
+                testCase.verifyTrue(false,ME.message);
+                end
+            end
+            
+            % Log the opened figures to the test reports
+            Figures = findall(groot,'Type','figure');
+            Figures = flipud(Figures);
+            if ~isempty(Figures)
+                for f = 1:size(Figures,1)
+                    FigDiag = matlab.unittest.diagnostics.FigureDiagnostic(Figures(f));
+                    log(testCase,1,FigDiag);
+                end
+            end
+            close all
+
+        end
+
+        function SimpleSmokeTest(testCase,Filename)
+
+            % Run the Smoke test
+            RootFolder = currentProject().RootFolder;
+            cd(RootFolder)
+            disp(">> Running " + Filename);
+            try
+                run(fullfile("Scripts",Filename));
+            catch ME
+                testCase.verifyTrue(false,ME.message);
+            end
+            
+            % Log the opened figures to the test reports
+            Figures = findall(groot,'Type','figure');
+            Figures = flipud(Figures);
+            if ~isempty(Figures)
+                for f = 1:size(Figures,1)
+                    try
+                    FigDiag = matlab.unittest.diagnostics.FigureDiagnostic(Figures(f));
+                    log(testCase,1,FigDiag);
+                    catch ME
+                        disp("Figures not logged ...")
+                    end
+                end
+            end
+            close all
+
+        end
     end
 
     methods (TestClassTeardown)
 
         function closeAllFigure(testCase)
             close all force  % Close figure windows
-            bdclose all      % Close Simulink models
         end
 
     end % methods (TestClassTeardown)
 
-end
-
-function [flag,MyErrorMessage,time] = SmokeTestMyFile(myFile)
-try
-    disp("Running " + myFile)
-    tic
-    run(myFile)
-    time = toc;
-catch ME
-    time = toc;
-    if myFile == "Introduction.mlx"
-        if ME.message == "Unrecognized function or variable 'Monday'."
-            flag = 0;
-            MyErrorMessage = "Expected error.";
-        else
-            flag = 1;
-            MyErrorMessage = ME.message;
-        end
-    else
-        flag = 1;
-        MyErrorMessage = ME.message;
-    end
-end
-if ~exist("flag","var")
-    flag = 0;
-    MyErrorMessage = "";
-end
 end
